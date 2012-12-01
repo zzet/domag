92d029833c141929c5737c1d96e5e23f15c2a3df
Andrey Kumanyaev <me@zzet.org>
2012-12-01 12:11:48 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Domag::Application.config.secret_token = '9db16fdd5dbadb42b7532894f188c6eba5ca73c595a17b643747d2d352190d07934bdb98a2e146a5b4a52a082df47ea4b9f29a6c34ff4ffd92cc95ed4240d0b1'
